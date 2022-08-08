3a578b8b57f9581101239f54fbaf13fc38124d9f
Ronen <101617329+Ronen6999@users.noreply.github.com>
2022-08-08 14:24:36 +0530

    Update Activate.ts

--- Activate.ts
+++ Activate.ts
@@ -1,4 +1,4 @@
-import MessageHandler from '../../Handlers/MessageHandler'
+import MessageHandler from '../../MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient, { toggleableGroupActions } from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
